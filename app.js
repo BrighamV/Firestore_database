653f0c9d9934681d68fa195c773f39c7a8f6cb53
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-01-22 11:32:34 -0700

    added update and minor styles

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
+// creating variables for later use
+
 const recipieList = document.querySelector('#recipies');
 const form = document.querySelector('#add-recipies');
 const find = document.querySelector('#find-recipie');
 const search = document.querySelector('#searchButton');
 const clear = document.querySelector('#clear');
-const show = document.querySelector("#everything");
+const show = document.querySelector('#everything');
 
 // create element and render
 
 function renderRecipie(doc){
+    // creating elements that will get inserted into our html
     let li = document.createElement('li');
     let name = document.createElement('span');
     let time = document.createElement('span');
     let min = document.createElement('span');
     let cross =document.createElement('button');
+    let update = document.createElement('button');
 
+    // adding info to our elements by grabbing data from our database
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     time.textContent = doc.data().time;
-    min.textContent = "Minutes";
-    cross.textContent = 'delete';
+    min.textContent = "Minutes ";
+    cross.textContent = ' delete ';
+    update.textContent = " update ";
 
+    // putting everything into a li
     li.appendChild(name);
     li.appendChild(time);
     li.appendChild(min);
     li.appendChild(cross);
+    li.appendChild(update);
 
+    // put everything into our ul
     recipieList.appendChild(li);
 
     //deleting data
@@ -33,23 +42,36 @@ function renderRecipie(doc){
         let id = e.target.parentElement.getAttribute('data-id');
         db.collection('recipes').doc(id).delete();
     })
+
+    // Updating data
+    update.addEventListener('click', (e) => {
+        let n = document.querySelector("#eName").value;
+        let t = document.querySelector("#eTime").value;
+
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).update({                                       // update will change the specified items in the database
+            name: n,
+            time: t
+        })
+    })
 }
 
+// this displays info according to the entered time
 function display() {
     let searchTime = document.querySelector('#times').value;
 
     console.log(searchTime);
-    db.collection('recipes').where("time", "==", searchTime).get().then((snapshot) => {   //get grabs the data from the database
+    db.collection('recipes').where("time", "==", searchTime).get().then((snapshot) => {   //where is used to query the database. takes 3 arguments
     snapshot.docs.forEach(doc => {
     renderRecipie(doc);
         })
     })
 }
 
+// this is for displaying every item in the database
 function display2() {
-    let searchTime = document.querySelector('#times').value;
 
-    console.log(searchTime);
     db.collection('recipes').get().then((snapshot) => {   //get grabs the data from the database
     snapshot.docs.forEach(doc => {
     renderRecipie(doc);
@@ -62,6 +84,7 @@ search.addEventListener('click', display);
 
 show.addEventListener('click', display2);
 
+// clearing out the ul
 clear.addEventListener('click', (e) => {
     document.getElementById('recipies').innerHTML = "";
 })
@@ -77,17 +100,3 @@ form.addEventListener('submit', (e) => {
     form.name.value = "";
     form.time.value = "";
 });
-
-// active listener for data
-// db.collection('recipies').orderBy('name').onSnapshot(snapshot => {
-//     let changes = snapshot.docChanges();
-//     console.log(changes);
-//     changes.forEach(change => {
-//         if(change.type == 'added'){
-//             renderRecipie(change.doc)
-//         } else if (change.type == 'removed') {
-//             let li = recipieList.querySelector('[data-id' + change.doc.id + ']');
-//             recipieList.removeChild(li);
-//         }
-//     });
-// });
